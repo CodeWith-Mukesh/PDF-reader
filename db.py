e919518c71e5275bdaf8f7e7158292b79d38c4d1
Mukesh <mukesh45yadav45g@gmail.com>
2026-04-11 13:40:48 +0530

    Making better

--- db.py
+++ db.py
@@ -8,9 +8,21 @@ import os
 import shutil
 
 EMBEDDING_MODEL = "all-MiniLM-L6-v2"
-CHROMA_DIR = f"chroma_db_{EMBEDDING_MODEL.replace('/', '_')}"
+# CHROMA_DIR = f"chroma_db_{EMBEDDING_MODEL.replace('/', '_')}"
+CHROMA_PATH = "chroma_db" 
 
 def vectorstore(pdf_path):
+    #This will delete all the old files and startb new chat
+    try:
+        old = Chroma(persist_directory=CHROMA_PATH, embedding_function=HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL))
+        old.delete_collection()  # ← collection ke andar ka data delete
+        print("Old collection deleted.")
+    except:
+        pass
+    if os.path.exists("chroma_db"):
+        shutil.rmtree("chroma_db", ignore_errors=True)
+        print("Old ChromaDB cleared.")
+    
     loader = PyPDFLoader(pdf_path)
     document = loader.load()
 
